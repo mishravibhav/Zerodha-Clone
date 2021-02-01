5056d21871443c6534fcbe9a53e7f0766fadf6d6
vibhav mishra <mishravibhav1910@gmail.com>
2021-02-02 01:32:27 +0530

    Zerodha clone

--- zerodha_clone/src/Components/Navbar.jsx
+++ zerodha_clone/src/Components/Navbar.jsx
@@ -3,15 +3,18 @@ import './Navbar.css'
 import Menuitems from "../menuitems.json"
 
 class NavBar extends React.Component{
+    state = {clicked:false}
+
+    handleclick = ()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
     render(){
         return(
             <nav className="Navbar-Items">
                     <img className="Navbar-logo" src="https://zerodha.com/static/images/logo.svg" alt="Zorodha-logo"/>
                 
-                <div className="menu-icon">
-                    
-                </div>
-                <ul>
+                
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {Menuitems.map((item,index)=>{
                         return(
                             <li key={index}><a href={item.url} className={item.cName}>{item.title}</a></li>
@@ -19,6 +22,10 @@ class NavBar extends React.Component{
                     })}
                     
                 </ul>
+
+                <div className="menu-icon" onClick={this.handleclick}>
+                    <li className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></li>
+                </div>
             </nav>
         )
     }
