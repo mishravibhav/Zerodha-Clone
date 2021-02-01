6e812214b1d65c25044de77286260eca46a79c9b
vibhav mishra <mishravibhav1910@gmail.com>
2021-02-01 23:55:17 +0530

    Zerodha clone

--- zerodha_clone/src/Components/Navbar.jsx
+++ zerodha_clone/src/Components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Navbar.css'
+import Menuitems from "../menuitems.json"
 
 class NavBar extends React.Component{
     render(){
         return(
             <nav className="Navbar-Items">
-                <h1 className="Navbar-logo"></h1>
-                <div className="menu-icon"></div>
+                    <img className="Navbar-logo" src="https://zerodha.com/static/images/logo.svg" alt="Zorodha-logo"/>
+                
+                <div className="menu-icon">
+                    
+                </div>
+                <ul>
+                    {Menuitems.map((item,index)=>{
+                        return(
+                            <li key={index}><a href={item.url} className={item.cName}>{item.title}</a></li>
+                        )
+                    })}
+                    
+                </ul>
             </nav>
         )
     }
