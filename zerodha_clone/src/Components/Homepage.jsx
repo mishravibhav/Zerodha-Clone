6f9402bf7924a903eefe2b6ff45f6f225b787b0e
vibhav mishra <mishravibhav1910@gmail.com>
2021-02-03 01:39:21 +0530

    Zerodha clone

--- zerodha_clone/src/Components/Homepage.jsx
+++ zerodha_clone/src/Components/Homepage.jsx
@@ -19,11 +19,32 @@ const Homepage = ()=>{
                 </div>
 
                 <div className="Largeststock__part__right">
-
+                    <h1 className="Largeststock__part__right__h1">Largest stock broker in India</h1>
+                    <p className="Largeststock__part__right__p">4+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+                    <div className="ul__container">
+                    <ul  className="Largeststock__part__right__ul">
+                        <li>Futures and Options</li>
+                        <li>Commodity derivatives</li>
+                        <li>Currency derivatives</li>
+                    </ul>
+                    <ul className="Largeststock__part__right__ul">
+                        <li>Stocks & IPOs</li>
+                        <li>Direct mutual funds</li>
+                        <li>Bonds and Govt. Securities</li>
+                    </ul>
+                    </div>
+                    <div className="">
+                        <img src="https://zerodha.com/static/images/press-logos.png" alt=""/>
+                    </div>
                 </div>
                
             </div>
-            <img src="https://zerodha.com/static/images/ecosystem.png" alt=""/>
+            <div className="Zeroidha_univ">
+                <h1>The Zerodha Universe</h1>
+                <p>A whole ecosystem of modern investment apps <br/> tailored to specific needs, built from the ground up</p>
+                <img src="https://zerodha.com/static/images/ecosystem.png" alt=""/>
+            </div>
+           
         </div>
     )
 }
