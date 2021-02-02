4679a573e6634464efd0b774f1e2b1dc0d6ce4bc
vibhav mishra <mishravibhav1910@gmail.com>
2021-02-02 23:14:49 +0530

    Zerodha homepage

--- zerodha_clone/src/App.js
+++ zerodha_clone/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      {/* <Navbar /> */}
     </div>
   );
 }
--- zerodha_clone/src/Components/Homepage.jsx
+++ zerodha_clone/src/Components/Homepage.jsx
@@ -5,11 +5,25 @@ const Homepage = ()=>{
     return(
         <div className="container">
             <Navbar/>
+           <div className="banner">
             <div className="container__no1broker" >
-                <img className="Banner__No1Broker" src="https://zerodha.com/static/images/landing.png" alt="invest in everything image"></img>
+                    <img className="Banner__No1Broker" src="https://zerodha.com/static/images/landing.png" alt="invest in everything image"></img>
+                </div>
+                <h1 className="heading__NO1Broker">Invest in everything</h1>
+                <p className="p__NO1Broker">Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                <button className="button__NO1Broker">Sign up Now</button>
+           </div>
+            <div className="Largeststock__part">
+                <div className="Largeststock__part__left">
+                        <img src="https://zerodha.com/static/images/largest-broker.svg" alt=""/>
+                </div>
+
+                <div className="Largeststock__part__right">
+
+                </div>
+               
             </div>
-            <h1 style={{textAlign:'center'}}>Invest in everything</h1>
-            
+            <img src="https://zerodha.com/static/images/ecosystem.png" alt=""/>
         </div>
     )
 }
